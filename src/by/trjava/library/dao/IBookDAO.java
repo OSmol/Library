1ae8dc4788b5d0164c2e35c34bec09b73b4f3b57
OSmol <Olga_Smolyakova@outlook.com>
2020-02-17 20:33:32 +0300

    Update IBookDAO.java

--- src/by/trjava/library/dao/IBookDAO.java
+++ src/by/trjava/library/dao/IBookDAO.java
@@ -2,7 +2,7 @@ package by.trjava.library.dao;
 
 import by.trjava.library.bean.Book;
 
-public interface IBookDAO{
+public interface IBookDAO{// именование интерфейсов с I уже устарело, лет 5-8 как устарело совсем
     void take(Book book) throws DAOException;
     void giveBack(Book book) throws DAOException;
     String getTakenBooks() throws DAOException;
